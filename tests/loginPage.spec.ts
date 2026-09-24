fd0d22f7a3962ad7c7fb64ecbe8b44a6dce62692
PoojaC <poojac@vrize.com>
2026-09-24 21:41:14 +0530

    Adding fixtures for context and page object manager

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -2,39 +2,43 @@ import test from "@playwright/test";
 // import { LoginPage } from "../pages/loginPage";
 // import { CategoryPage } from "../pages/categoryPage";
 // import { ProductDetailsPage } from "../pages/productDetailsPage";
-import { PageObjectManager } from "../pages/pageObjectManager";
+// import { PageObjectManager } from "../pages/pageObjectManager";
 import { testUsers } from "../testdata/credentials";
+import { testBase } from "../utils/fixtures/contextFixture";
 
 for (const user of testUsers) {
-  test(`Login actions - ${user.userName}`, async ({ browser }) => {
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  testBase(
+    `Login actions - ${user.userName}`,
+    async ({ BaseContext, PageObjectManager }) => {
+      // const context = await browser.newContext();
+      // const page = await context.newPage();
 
-    // let userName = process.env.USERNAME!;
-    // let password = process.env.PASSWORD!;
-    let productName = "Sauce Labs Backpack";
+      // let userName = process.env.USERNAME!;
+      // let password = process.env.PASSWORD!;
+      let productName = "Sauce Labs Backpack";
 
-    // const loginPage = new LoginPage(page);
-    // await loginPage.goTo();
-    // await loginPage.loginMethod(userName, password);
+      // const loginPage = new LoginPage(page);
+      // await loginPage.goTo();
+      // await loginPage.loginMethod(userName, password);
 
-    // const productPage = new CategoryPage(page);
-    // await productPage.searchProduct(productName);
+      // const productPage = new CategoryPage(page);
+      // await productPage.searchProduct(productName);
 
-    // const productDetailsPage = new ProductDetailsPage(page);
-    // await productDetailsPage.verifyProductDetails(productName);
+      // const productDetailsPage = new ProductDetailsPage(page);
+      // await productDetailsPage.verifyProductDetails(productName);
 
-    const pageObjectManagerObject = new PageObjectManager(page);
+      // const pageObjectManagerObject = new PageObjectManager(BaseContext);
 
-    const loginPage = await pageObjectManagerObject.getLoginPage();
-    await loginPage.goTo();
-    await loginPage.loginMethod(user.userName, user.password);
+      const loginPage = await PageObjectManager.getLoginPage();
+      await loginPage.goTo();
+      await loginPage.loginMethod(user.userName, user.password);
 
-    const categoryPage = await pageObjectManagerObject.getCategoryPage();
-    await categoryPage.searchProduct(productName);
+      const categoryPage = await PageObjectManager.getCategoryPage();
+      await categoryPage.searchProduct(productName);
 
-    const productDetailsPage =
-      await pageObjectManagerObject.getProductDetailsPage();
-    await productDetailsPage.verifyProductDetails(productName);
-  });
+      const productDetailsPage =
+        await PageObjectManager.getProductDetailsPage();
+      await productDetailsPage.verifyProductDetails(productName);
+    }
+  );
 }
